77be4c3859fd15f3ebc6b8b2d37bf2c3e880aeb4
Irene Novo <irenenovo@MacBook-Air-de-entelibai.local>
2015-04-28 13:54:49 +0200

    Nuevos teses y funcionalidad

---
+++ public_html/js/calculator_api.js
@@ -0,0 +1,111 @@
+
+var model = {
+    whiteVotes: 0,
+    seats: 0,
+    strip: 0,
+    votes: []
+};
+
+var octopus = {
+    checkVotes: function (vote) {
+        var minVotes = this.getTotalVotes() * 0.05;
+        if (vote.votes >= minVotes) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    clearData: function () {
+        model.whiteVotes = 0;
+        model.seats = 0;
+        model.strip = 0;
+        model.votes = [];
+    },
+    getVotes: function () {
+        return model.votes;
+    },
+    getTotalVotes: function () {
+        var totalVotes = 0;
+        for (var i = 0; i < model.votes.length; i++) {
+            totalVotes += model.votes[i].votes;
+        }
+        totalVotes += this.getWhiteVotes();
+        return totalVotes;
+    },
+    getWhiteVotes: function () {
+        return model.whiteVotes;
+    },
+    setWhiteVotes: function (newWhiteVotes) {
+        model.whiteVotes = newWhiteVotes;
+    },
+    insertVote: function (vote) {
+        model.votes.push(vote);
+    },
+    getVote: function (index) {
+        return model.votes[index];
+    },
+    getStringVote: function (index) {
+        return "Partido: " + model.votes[index].name + ", votos: " + model.votes[index].votes;
+    },
+    getVotesLenght: function () {
+        return model.votes.length;
+    },
+    deleteVote: function (index) {
+        model.votes.splice(index, 1);
+    },
+    getSeats: function () {
+        return model.seats;
+    },
+    setSeats: function (newSeats) {
+        model.seats = newSeats;
+    },
+    getStrip: function () {
+        return model.strip;
+    },
+    setStrip: function (newStrip) {
+        model.strip = newStrip;
+    },
+    getSeatVotes: function () {
+        var arr = [];
+        for (var i = 0; i < model.votes.length; i++) {
+            var vote = model.votes[i];
+            //comprobamos que pase el umbral mínimo para añadirlo al array
+            if (this.checkVotes(vote)) {
+                console.log("NO ignora los votos de " +vote.name);
+                for (var j = 1; j <= model.seats; j++) {
+                    var divVote = vote.votes / j;
+
+                    arr.push([j, vote.name, divVote]);
+                }
+            }else{
+                console.log("ignora los votos de " +vote.name);
+            }
+        }
+
+        //lo ordenamos por el número de votos
+        arr.sort((function (index) {
+            return function (a, b) {
+                var w = (a[index] === b[index] ? 0 : (a[index] < b[index] ? -1 : 1));
+                console.log("devuelve " + w);
+                return w;
+            };
+        })(2));
+
+        console.log(arr);
+
+        var newArr = {};
+
+        //ahora deberemos coger los 5 más altos
+        for (var i = 0; i < this.getSeats(); i++) {
+            var v = arr.pop();
+            var name = v[1];
+            if ((newArr).hasOwnProperty(name)) {
+                newArr[name] += 1;
+            } else {
+                newArr[name] = 1;
+            }
+        }
+        console.log(newArr);
+        return newArr;
+    }
+};
