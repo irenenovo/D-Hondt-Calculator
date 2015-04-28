1f78845d302f965f5780dcc785db642fa4514202
Irene Novo <irenenovo@MacBook-Air-de-entelibai.local>
2015-04-28 13:53:37 +0200

    Nuevos teses y funcionalidad

--- public_html/js/calculator.js
+++ public_html/js/calculator.js
@@ -1,109 +1,61 @@
 
-//$(function () {
+$(function () {
 
-var model = {
-    whiteVotes: 0,
-    seats: 0,
-    strip: 0,
-    votes: []
-};
+    /*var model = {
+     whiteVotes: 0,
+     seats: 0,
+     strip: 0,
+     votes: []
+     };
+     
+     var octopus = {
+     
+     };*/
 
-var octopus = {
-    checkVotes: function (vote) {
-        var minVotes = this.getTotalVotes() * 0.05;
-        if (vote.votes >= minVotes) {
-            return true;
-        } else
-            return false;
-    },
-    getVotes: function () {
-        return model.votes;
-    },
-    getTotalVotes: function () {
-        var totalVotes = 0;
-        for (var i = 0; i < model.votes.length; i++) {
-            totalVotes += model.votes[i].votes;
-        }
+    var view = {
+        init: function () {
+            $("#calculator").on("submit", function (e) {
+                e.preventDefault();
+                var wVotes = parseInt($("#white_votes").val());
+                var strip = $("#strip").val();
+                var seats = $("#seats").val();
 
-        totalVotes += this.getWhiteVotes();
+                octopus.setWhiteVotes(wVotes);
+                octopus.setStrip(strip);
+                octopus.setSeats(seats);
 
-        return totalVotes;
-    },
-    getWhiteVotes: function () {
-        return model.whiteVotes;
-    },
-    setWhiteVotes: function (newWhiteVotes) {
-        model.whiteVotes = newWhiteVotes;
-    },
-    insertVote: function (vote) {
-        model.votes.push(vote);
-    },
-    getVote: function (index) {
-        return model.votes[index];
-    },
-    getStringVote: function(index){
-        return "Partido: "+model.votes[index].name + ", votos: " + model.votes[index].votes;
-    },
-    getVotesLenght: function(){
-        return model.votes.length;
-    },
-    deleteVote: function (index) {
-        model.votes.splice(index, 1);
-    },
-    getSeats: function(){
-        return model.seats;
-    },
-    setSeats: function(newSeats){
-        model.seats = newSeats;
-    },
-    getStrip: function(){
-        return model.strip;
-    },
-    setStrip: function(newStrip){
-        model.strip = newStrip;
-    }
-};
-
-var view = {
-    init: function () {
-        $("#calculator").on("submit", function (e) {
-            e.preventDefault();
-            var wVotes = parseInt($("#white_votes").val());
-            var strip = $("#strip").val();
-            var seats = $("#seats").val();
-            
-            octopus.setWhiteVotes(wVotes);
-            octopus.setStrip(strip);
-            octopus.setSeats(seats);
-                        
-            $("#explain").empty();
-            $("#result").empty();
-            var html = "Resultados: ";
-            for(var i = 0; i< octopus.getVotesLenght(); i++){
-                var vote = octopus.getVote(i);
-                if(octopus.checkVotes(vote)){
-                    html+= "</br>El partido "+vote.name + " obtiene escaños";
-                }else{
-                    html+= "</br>El partido "+vote.name + " NO obtiene escaños por no llegar al listón electoral.";
+                $("#explain").empty();
+                $("#result").empty();
+                var html = "Resultados: ";
+                for (var i = 0; i < octopus.getVotesLenght(); i++) {
+                    var vote = octopus.getVote(i);
+                    if (octopus.checkVotes(vote)) {
+                        html += "</br>El partido " + vote.name + " obtiene escaños";
+                    } else {
+                        html += "</br>El partido " + vote.name + " NO obtiene escaños por no llegar al listón electoral.";
+                    }
                 }
-            }
-            $("#result").append(html);
-        });
 
-        $("#addNew").on("click", function () {
-            var name = $("#name").val();
-            var votes = parseInt($("#votes").val());
-            var vote = {name: name, votes: votes};
-            octopus.insertVote(vote);
-            
-            $("#name").val("");
-            $("#votes").val("");
-            
-            $("#explain").append("<div>"+octopus.getStringVote(octopus.getVotesLenght()-1)+ "</div>"); 
-        });
-    }
-};
+                var res = JSON.stringify(octopus.getSeatVotes());
+                console.log(res);
 
-view.init();
+                $("#result").append(res);
+            });
 
-//}());
+            $("#addNew").on("click", function () {
+                var name = $("#name").val();
+                var votes = parseInt($("#votes").val());
+                var vote = {name: name, votes: votes};
+                octopus.insertVote(vote);
+
+                $("#name").val("");
+                $("#votes").val("");
+
+                $("#explain").append("<div>" + octopus.getStringVote(octopus.getVotesLenght() - 1) + "</div>");
+            });
+        }
+    };
+
+    view.init();
+
+}());
--- public_html/spec/calculatorSpec.js
+++ public_html/spec/calculatorSpec.js
@@ -1,6 +1,6 @@
 
 describe('Votes', function () {
-    
+
     var vote;
 
     beforeEach(function () {
@@ -8,7 +8,7 @@ describe('Votes', function () {
         octopus.setWhiteVotes(100);
         octopus.setStrip(5);
         octopus.setSeats(100);
-        vote = { name: "PSOE", votes: 100 };
+        vote = {name: "PSOE", votes: 100};
     });
 
     //caso 1: comprobar que los datos estén bien metidos
@@ -36,21 +36,72 @@ describe('Votes', function () {
 });
 
 describe('Calculator', function () {
-    
-    var vote;
+
+    var vote1, vote2;
 
     beforeEach(function () {
+
+        octopus.clearData();
+
         //inicializamos con set de datos
         octopus.setWhiteVotes(100);
         octopus.setStrip(5);
-        octopus.setSeats(100);
-        vote = {name: "PSOE", votes: 100};
+        octopus.setSeats(5);
+
     });
 
     //caso 1: descartar partido si tiene menos de 5% de los votos totales + blancos
-    it('should discard votes with less than '+ octopus.getStrip() +'%', function () {
+    it('should discard votes with less than ' + octopus.getStrip() + '%', function () {
+        vote1 = {name: "PP", votes: 300};
+        octopus.insertVote(vote1);
+        expect(octopus.checkVotes(vote1)).toBe(true);
+    });
 
-        expect(octopus.checkVotes(vote)).toBe(true);
+    //caso 2: caso en el que se reparten bien los escaños
+    it('should return the seats well', function () {
+
+        //votos de prueba
+        vote1 = {name: "PP", votes: 300};
+        vote2 = {name: "PSOE", votes: 420};
+
+        octopus.insertVote(vote1);
+        octopus.insertVote(vote2);
+
+        //resultado que esperamos
+        var result = {PSOE: 3, PP: 2};
+
+        expect(octopus.getSeatVotes()).toEqual(result);
+    });
+
+    //caso 3: caso de empate -> debería haber sorteo
+    it('should return draw', function () {
+
+        //votos de prueba
+        vote1 = {name: "PP", votes: 100};
+        vote2 = {name: "PSOE", votes: 200};
+
+        octopus.insertVote(vote1);
+        octopus.insertVote(vote2);
+
+        //expect(octopus.getSeatVotes()).toBe("Sorteo");
+
+    });
+
+    //caso 3: caso de empate -> debería haber sorteo
+    it('should ignore PP votes', function () {
+
+        //votos de prueba
+        vote1 = {name: "PP", votes: 10};
+        vote2 = {name: "Podemos", votes: 1000};
+
+        octopus.insertVote(vote1);
+        octopus.insertVote(vote2);
+
+        //resultado que esperamos
+        var result = {Podemos: 5};
+
+        expect(octopus.getSeatVotes()).toEqual(result);
+
     });
 
 });
